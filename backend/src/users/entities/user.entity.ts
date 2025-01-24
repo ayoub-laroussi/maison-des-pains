666c7ad793ada972cdd6164a79830003e9faa192
Ayoub Laroussi <ayoublaroussipro@gmail.com>
2025-01-24 21:55:47 +0100

    feat(users): ajouter les décorateurs de validation pour l'entité utilisateur

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { Exclude } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsString, MinLength, IsEnum, IsOptional, IsPhoneNumber } from 'class-validator';
 
 export enum UserRole {
   CLIENT = 'CLIENT',
@@ -13,19 +14,29 @@ export class User {
   id: string;
 
   @Column({ unique: true })
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @Column()
+  @IsNotEmpty()
+  @MinLength(8)
   @Exclude()
   password: string;
 
   @Column()
+  @IsString()
+  @IsNotEmpty()
   firstName: string;
 
   @Column()
+  @IsString()
+  @IsNotEmpty()
   lastName: string;
 
   @Column({ nullable: true })
+  @IsOptional()
+  @IsPhoneNumber('FR')
   phoneNumber: string;
 
   @Column({
@@ -33,6 +44,7 @@ export class User {
     enum: UserRole,
     default: UserRole.CLIENT,
   })
+  @IsEnum(UserRole)
   role: UserRole;
 
   @Column({ default: true })
